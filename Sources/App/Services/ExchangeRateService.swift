704569bbabe2558211583ab5d24a97c2a4a94de7
Alexandr Goncharov <ag@bekitzur.com>
2019-04-11 00:02:43 +0300

    Refactor response parsing

--- Sources/App/Services/ExchangeRateService.swift
+++ Sources/App/Services/ExchangeRateService.swift
@@ -23,7 +23,12 @@ final class ExchangeRateService: ExchangeRateServiceProtocol {
             completion(.failure(.invalidRequest))
             return Disposable.empty
         }
-        let transform: (CurrencyPairDictionary<ExchangeRate.Rate>) -> [ExchangeRate] = { dictionary in
+        let transform: ([String: ExchangeRate.Rate]) throws -> [ExchangeRate] = { rawDictionary in
+            let dictionary: [CurrencyPair: ExchangeRate.Rate] = try Dictionary(uniqueKeysWithValues: rawDictionary
+                .map { code, rate in
+                    let pair = try CurrencyPair(rawValue: code)
+                    return (pair, rate)
+            })
             return pairs.map { pair in
                 let rate: ExchangeRate.Rate = dictionary[pair] ?? 0.0
                 return ExchangeRate(rate: rate, currencies: pair)
@@ -42,57 +47,5 @@ private final class GetRatesRequest: HTTPRequest {
             URLQueryItem(name: "pairs", value: $0.rawValue)
         }
         super.init(path: "revolut-ios", query: queryItems)
-    }
-}
-
-/// Wrap `Dictionary` with `CurrencyPair` as a key.
-///
-/// required due to the way how `Decodable` is implemented in the `Dictionary`
-///
-/// see [implementation](https://github.com/apple/swift/blob/master/stdlib/public/core/Codable.swift.gyb)
-/// for more details
-///
-private struct CurrencyPairDictionary<Value: Decodable>: Sequence, Decodable {
-
-    typealias Storage = [CurrencyPair: Value]
-    typealias Iterator = Storage.Iterator
-    private var storage = Storage()
-
-    func makeIterator() -> Iterator {
-        return storage.makeIterator()
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CurrencyPairDictionaryCodingKey.self)
-        for rawKey in container.allKeys {
-            let key = try CurrencyPair(rawValue: rawKey.stringValue)
-            let value = try container.decode(Value.self, forKey: rawKey)
-            storage[key] = value
-        }
-    }
-
-    subscript(key: Storage.Key) -> Storage.Value? {
-        return storage[key]
-    }
-
-    /// convenience constructor
-    init(_ values: Storage) {
-        storage = values
-    }
-
-}
-
-/// Private `CodingKey` used in the `CurrencyPairDictionary`
-private struct CurrencyPairDictionaryCodingKey: CodingKey {
-    internal let stringValue: String
-    internal let intValue: Int?
-
-    internal init?(stringValue: String) {
-        self.stringValue = stringValue
-        self.intValue = Int(stringValue)
-    }
-    internal init?(intValue: Int) {
-        self.stringValue = "\(intValue)"
-        self.intValue = intValue
     }
 }
