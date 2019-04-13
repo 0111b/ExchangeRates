9aa7bae17fa05e87fffa3e1e14532fe1215a3bd7
Alexandr Goncharov <adanfermer@gmail.com>
2019-04-14 01:47:41 +0300

    App coordinator tests

---
+++ Tests/App/AppCoordinatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  AppCoordinatorTests.swift
+//  ExchangeRatesTests
+//
+//  Created by Alexandr Goncharov on 14/04/2019.
+//  Copyright © 2019 Revolut. All rights reserved.
+//
+
+import XCTest
+@testable import ExchangeRates
+import UIKit
+
+class AppCoordinatorTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testWindowIsKey() {
+        let window = WindowStub()
+        let coordinator = AppCoordinator(mainWindow: window)
+        coordinator.start()
+        XCTAssertTrue(window.makeKeyAndVisibleCalled)
+    }
+}
+
+private class WindowStub: UIWindow {
+    var makeKeyAndVisibleCalled = false
+    override func makeKeyAndVisible() {
+        makeKeyAndVisibleCalled = true
+    }
+}
